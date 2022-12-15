8f4efa12cc3692dfa1a3ee63aeff96a5f9a1a1cd
rozet <pinrozeth@gmail.com>
2022-12-15 14:08:03 +0700

    NFT-Collection

---
+++ Hardhat-Tutorial/arguments.ts
@@ -0,0 +1,4 @@
+module.exports = [
+    "https://nft-collection-five-red.vercel.app/api/",
+    "0xD9522fC100939027410dDe73785578BaFB14F138"
+]
--- Hardhat-Tutorial/constants/index.ts
+++ Hardhat-Tutorial/constants/index.ts
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
-const WHITELIST_CONTRACT_ADDRESS = "0x4AFD939643d0E782d2DA61a18a9e7e6661af3cD5"
+const WHITELIST_CONTRACT_ADDRESS = "0xD9522fC100939027410dDe73785578BaFB14F138"
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/"
+const METADATA_URL = "https://nft-collection-five-red.vercel.app/api/"
 
 module.exports = {WHITELIST_CONTRACT_ADDRESS, METADATA_URL};
